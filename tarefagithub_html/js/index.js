d2feb353d216ec1e95c16e9406c008fa9b98dee7
RenatoOsmar <renatoosmar19@gmail.com>
2020-03-03 21:21:20 -0300

    modifiquei os arquivos index.html index.js

---
+++ tarefagithub_html/js/index.js
@@ -0,0 +1,6 @@
+var minhaCasa = ['Renato' ,'Ayane' ,'João Vitor' ,'Lara'];
+for(i=0; i=minhaCasa.length;i++){
+    minhaCasa[i];
+}
+var res = document.getElementById('res');
+res.textContent = minhaCasa;
